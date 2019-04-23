f0f58264ce837b9d0327c6704136339ea561477e
Amarilis Camargo <amarilis.camargo@lambda3.com.br>
2019-04-23 18:18:52 -0300

    Criando Primeira Rota

---
+++ server.js
@@ -0,0 +1,13 @@
+// Importando express
+const express = require("express");
+// Instanciando a função do express
+app = express();
+
+// Rota
+app.get("/", (req,res) => {
+ // send() : Envio de dados genérico
+   res.send("Hello World1");
+});
+
+// Definindo porta localhost
+app.listen(3000);
