cccd1c696a21ace21ec9c2621c6c2f2e10fe6d99
Amarilis Camargo <amarilis.camargo@lambda3.com.br>
2019-04-23 19:07:46 -0300

    Conectando Banco de Dados

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-// Importando express
+// Importando Dependências
 const express = require("express");
-// Instanciando a função do express
-app = express();
+const mongoose = require("mongoose");
 
-// Rota
+// Iniciando o App
+const app = express();
+
+// Iniciando o DB
+mongoose.connect("mongodb://localhost:27017/nodeapi",{useNewUrlParser: true});
+
+// Primeira Rota
 app.get("/", (req,res) => {
- // send() : Envio de dados genérico
-   res.send("Hello World");
+    // send() : Envio de dados genérico
+    res.send("Hello World");
 });
 
 // Definindo porta localhost
 app.listen(3000);
+
