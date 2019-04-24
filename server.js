99f0249dc8cdc534adbddee6dac1abbdd2d0631d
Amarilis Camargo <amarilis.camargo@lambda3.com.br>
2019-04-23 23:18:54 -0300

    Criando Models

--- server.js
+++ server.js
@@ -2,16 +2,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const requireDir = require("require-dir");
+
 // Iniciando o App
 const app = express();
 
 // Iniciando o DB
-mongoose.connect("mongodb://localhost:27017/nodeapi",{useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/nodeapi",{useNewUrlParser: true});
 
+const url = 'mongodb+srv://highfive:******@clusterapi-yygke.mongodb.net/test?retryWrites=true';
+const options = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true };
+mongoose.connect(url, options);
+
+requireDir("./src/models");
+
+
+const Book = mongoose.model('Book');
 // Primeira Rota
 app.get("/", (req,res) => {
     // send() : Envio de dados genérico
-    res.send("Hello World");
+    //res.send("Hello World");
+    Book.create({
+      title: 'Condenada',
+      description: 'Menina chata vai para o inferno',
+      author: 'Chuck Palahniuk'
+    })
 });
 
 // Definindo porta localhost
---
+++ src/models/Book.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const BookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        defaut: Date.now
+    },
+});
+
+mongoose.model('Book', BookSchema);
+
